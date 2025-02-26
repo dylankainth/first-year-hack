62aea67c2a2c2d64c016a2f6b005deb789e691bc
Dylan Kainth <50585244+dylankainth@users.noreply.github.com>
2025-02-26 19:46:49 +0000

    loosened middleware to allow webmanifest

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -4,7 +4,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
 
     const authToken = useCookie("auth_token").value;
 
-    if (!authToken && to.path !== "/welcome" && to.path !== "/auth/callback" && !to.path.startsWith("/api")) {
+    if (!authToken && to.path !== "/welcome" && to.path !== "/auth/callback" && !to.path.startsWith("/api") && !to.path.endsWith(".webmanifest") && !to.path.endsWith(".png") && !to.path.endsWith(".json")) {
         return navigateTo("/welcome");
     }
 });
