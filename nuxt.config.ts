276bd1f81cb96a9e0496ec7c394e80df04265f84
Dylan Kainth <50585244+dylankainth@users.noreply.github.com>
2025-02-26 13:48:05 +0000

    setup pwa base template

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@vite-pwa/nuxt'],
+  pwa: {
+    manifest: {
+      name: 'WalkieTalkie',
+      short_name: 'WalkieTalkie',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#ffffff',
+      description: 'Your AI Tour Guide',
+      lang: 'en',
+      icons: [
+        {
+          src: '/logo.svg',
+          sizes: '192x192',
+          type: 'image/svg',
+        },
+
+      ]
+    }
+  }
+
 })
