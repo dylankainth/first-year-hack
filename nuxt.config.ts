42aedd938b246c2ab0d3b4205fc50f0b4eaf2c4f
Dylan Kainth <50585244+dylankainth@users.noreply.github.com>
2025-02-26 19:36:52 +0000

    added pwa workbox

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,19 @@ export default defineNuxtConfig({
   },
   css: ['~/assets/css/main.css'],
   pwa: {
+    workbox: {
+      globPatterns: ["**/*.{js,css,html,png,svg,ico,json}"], // Ensure it caches HTML
+      runtimeCaching: [
+        {
+          urlPattern: "/",
+          handler: "NetworkFirst", // Fallback to network if not precached
+          options: {
+            cacheName: "html-pages",
+            expiration: { maxEntries: 10, maxAgeSeconds: 24 * 60 * 60 }, // 1-day cache
+          },
+        },
+      ],
+    },
     manifest: {
       name: 'KingsQuest',
       short_name: 'KingsQuest',
